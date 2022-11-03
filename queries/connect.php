733a89619a6de2e34e6b8f3d97baf040aae13a0b
AbigaelKirwa <abigael.kirwa@strathmore.edu>
2022-11-03 16:52:56 +0300

    modified to have heroku configuration

--- queries/connect.php
+++ queries/connect.php
@@ -1,6 +1,17 @@
 <?php
 //create a connection
-$conn = new mysqli("localhost", "root", "", "matatudb",3307);
+//$conn = new mysqli("localhost", "root", "", "matatudb",3307);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+$cleardb_username = $cleardb_url["b6797b4da26ff2"];
+$cleardb_password = $cleardb_url["eeac73ee"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 //check connection
 if($conn -> connect_error)
